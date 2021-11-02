5a9d90fa484b19cce4bf52c1360d5793609857db
Davide Scrivano <93290216+DavideUnical@users.noreply.github.com>
2021-11-02 18:25:07 +0100

    v0.1

---
+++ Es1IngSwCode/src/main/java/DavideScrivano/Es1IngSwCode/App.java
@@ -0,0 +1,7 @@
+package DavideScrivano.Es1IngSwCode;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Esercizio1");
+    }
+}
---
+++ Es1IngSwCode/src/test/java/DavideScrivano/Es1IngSwCode/AppTest.java
@@ -0,0 +1,15 @@
+package DavideScrivano.Es1IngSwCode;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+        System.out.println("Test eseguito");
+    }
+}
